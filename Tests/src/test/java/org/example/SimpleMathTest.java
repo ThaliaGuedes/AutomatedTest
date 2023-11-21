a7ff4127f4e167c3dd61756b04856a1ab2aa29d9
ThaliaGuedes <thaliaguedes1199@gmail.com>
2023-11-21 13:08:42 -0300

    rising division tests

--- Tests/src/test/java/org/example/SimpleMathTest.java
+++ Tests/src/test/java/org/example/SimpleMathTest.java
@@ -25,4 +25,17 @@ class SimpleMathTest {
       assertEquals(expected, actual);
     }
 
+    @Test
+    void testDivisao(){
+        SimpleMath math = new SimpleMath();
+
+        double firstNumber = 25D;
+        double secondNumber = 5D;
+        Double actual = math.division(firstNumber, secondNumber);
+        double expected = 5;
+
+        //lambda so sera executado caso o teste falhe
+        assertEquals(expected, actual, () -> firstNumber + "/" + secondNumber +
+                "did not produce" + expected + "!");
+    }
 }
